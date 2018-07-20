ade3a08416fff380b3cf4b3e252dc7cc26455b21
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-07-20 23:12:47 +0200

    Update entrypoint and dockery shell scripts with safer options * -e exit immediately upon error * -u treat unset variables as error * -o pipefail better error handling for pipes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,6 @@
 # @author Michael Wiesendanger <michael.wiesendanger@gmail.com>
 # @description launch script for confluence
 
-# abort when trying to use unset variable
-set -o nounset
+set -euo pipefail
 
 exec su-exec ${CONFLUENCE_USER} /opt/atlassian/confluence/bin/catalina.sh run
